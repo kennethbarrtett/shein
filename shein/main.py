1a5f808922da3452939c1ec81c7672de4ef1bb31
kennethbarrtett <44135632+kennethbarrtett@users.noreply.github.com>
2018-11-01 03:51:56 -0400

    Update main.py

--- shein/main.py
+++ shein/main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.redirect("https://www.aliexpress.com/", True)
+        self.redirect("https://tracking.vcommission.com/aff_c?offer_id=6814&aff_id=77927", True)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
